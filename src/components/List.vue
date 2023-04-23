005917d62ac6bb7532bf90c06c4178e25ae7def0
valmri <valmarmie87@outlook.com>
2023-04-23 13:52:28 +0200

    Édition film

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,15 +3,26 @@
 
       <div v-for="(film, index) in films" class="d-flex justify-content-between align-items-center bg-white text-dark rounded p-3 m-4">
         <span class="fw-bold">{{ film }}</span>
-        <button class="btn btn-danger" title="Supprimer le film" @click="deleteFilm(index)">
-          <i class="las la-trash"></i>
-        </button>
+        <div>
+          <button class="btn btn-primary mx-2" title="Éditer le film" @click="openCardEditFilm(index)">
+            <i class="las la-edit"></i>
+          </button>
+          <button class="btn btn-danger" title="Supprimer le film" @click="deleteFilm(index)">
+            <i class="las la-trash"></i>
+          </button>
+        </div>
       </div>
 
       <div class="bg-white text-dark rounded p-3 m-4" id="cardNewFilm" v-show="openCard">
         <span class="fw-bold">Nouveau film</span>
         <input type="text" class="form-control mt-2 mb-3" v-model="titreFilm">
         <button class="btn btn-success w-100" @click="addFilm">Enregistrer</button>
+      </div>
+
+      <div class="bg-white text-dark rounded p-3 m-4" id="cardEditFilm" v-show="openCardEdit">
+        <span class="fw-bold">Éditer un film</span>
+        <input type="text" class="form-control mt-2 mb-3" v-model="titreFilm" placeholder="titreFilm">
+        <button class="btn btn-success w-100" @click="editFilm(indexFilm)">Enregistrer</button>
       </div>
       <button class="btn btn-primary p-2 m-4" @click="openCardNewFilm" v-show="!openCard">Ajouter un film</button>
   </div>
@@ -28,6 +39,8 @@ export default {
           'Fast and furious'
       ],
       openCard: false,
+      openCardEdit: false,
+      indexFilm: null,
       titreFilm: ''
     }
   },
@@ -44,6 +57,15 @@ export default {
       if (index !== -1) {
         this.films.splice(index, 1);
       }
+    },
+    openCardEditFilm: function(index) {
+      this.openCardEdit = true;
+      this.titreFilm = this.films[index];
+      this.indexFilm = index;
+    },
+    editFilm: function(index) {
+      this.films[index] = this.titreFilm;
+      this.openCardEdit = false;
     }
   }
 }
