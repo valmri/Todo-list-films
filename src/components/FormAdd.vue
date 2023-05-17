99b485130ff187ea03d9bb45dbc25f830563133d
valmri <valmarmie87@outlook.com>
2023-05-17 16:15:42 +0200

    Édition film

--- src/components/FormAdd.vue
+++ src/components/FormAdd.vue
@@ -23,282 +23,32 @@
 
     <button class="btn btn-success w-100" @click="addFilm">Enregistrer</button>
   </div>
-
-  <div class="bg-white text-dark rounded p-3 m-4" id="cardEditFilm" ref="cardEditFilm" v-show="openCardEdit">
-    <span class="fw-bold">Éditer un film</span>
-    <input type="text" class="form-control mt-2 mb-3" v-model="film.nom" placeholder="Titre du film">
-    <select class="form-control mt-2 mb-3" v-model="genreFilm">
-      <option disabled value="">Genre</option>
-      <option v-for="(genre) in genres" :value="genre">{{ genre }}</option>
-    </select>
-    <input type="text" class="form-control mt-2 mb-3" v-model="anneeFilm" placeholder="Année">
-    <select class="form-control mt-2 mb-3" v-model="langueFilm">
-      <option disabled value="">Langue</option>
-      <option v-for="(langue) in langues" :value="langue">{{ langue }}</option>
-    </select>
-    <hr/>
-    <span class="fw-bold">Réalisateur</span>
-    <input type="text" class="form-control mt-2 mb-3" v-model="nomRealisateur" placeholder="Nom">
-    <input type="text" class="form-control mt-2 mb-3" v-model="prenomRealisateur" placeholder="Prénom">
-    <select class="form-control mt-2 mb-3" v-model="nationaliteRealisateur">
-      <option disabled value="">Nationalité</option>
-      <option v-for="(nationalite) in nationalites" :value="nationalite">{{ nationalite }}</option>
-    </select>
-    <input type="text" class="form-control mt-2 mb-3" v-model="naissanceRealisateur" placeholder="Année de naissance">
-
-    <button class="btn btn-success w-100" @click="editFilm(indexFilm)">Enregistrer</button>
-  </div>
-
   <div class="text-center p-3 m-4">
     <button class="btn btn-success w-25" @click="openCardNewFilm" v-show="!openCard">Ajouter un film</button>
   </div>
-
 </template>
 
 <script>
 export default {
-  name: "Form",
+  name: "FormAdd",
   emits: ['film-added'],
+  props: {
+    genres: {
+      type: Array,
+      default: null
+    },
+    langues: {
+      type: Array,
+      default: null
+    },
+    nationalites: {
+      type: Array,
+      default: null
+    }
+  },
   data() {
     return {
-      genres: [
-        "Action",
-        "Aventure",
-        "Animation",
-        "Biographie",
-        "Comédie",
-        "Comédie dramatique",
-        "Comédie musicale",
-        "Documentaire",
-        "Drame",
-        "Epouvante-horreur",
-        "Fantaisie",
-        "Guerre",
-        "Historique",
-        "Policier",
-        "Romance",
-        "Science-fiction",
-        "Thriller",
-        "Western"
-      ],
-      langues: [
-        "Anglais",
-        "Français",
-        "Espagnol",
-        "Allemand",
-        "Italien",
-        "Japonais",
-        "Mandarin",
-        "Cantonais",
-        "Coréen",
-        "Russe",
-        "Portugais",
-        "Arabe",
-        "Hindi",
-        "Bengali",
-        "Tamil",
-        "Télougou",
-        "Marathi",
-        "Punjabi",
-        "Gujarati"
-      ],
-      nationalites: [
-        "Afghan",
-        "Albanian",
-        "Algerian",
-        "American",
-        "Andorran",
-        "Angolan",
-        "Antiguans",
-        "Argentinean",
-        "Armenian",
-        "Australian",
-        "Austrian",
-        "Azerbaijani",
-        "Bahamian",
-        "Bahraini",
-        "Bangladeshi",
-        "Barbadian",
-        "Barbudans",
-        "Batswana",
-        "Belarusian",
-        "Belgian",
-        "Belizean",
-        "Beninese",
-        "Bhutanese",
-        "Bolivian",
-        "Bosnian",
-        "Brazilian",
-        "British",
-        "Bruneian",
-        "Bulgarian",
-        "Burkinabe",
-        "Burmese",
-        "Burundian",
-        "Cambodian",
-        "Cameroonian",
-        "Canadian",
-        "Cape Verdean",
-        "Central African",
-        "Chadian",
-        "Chilean",
-        "Chinese",
-        "Colombian",
-        "Comoran",
-        "Congolese",
-        "Costa Rican",
-        "Croatian",
-        "Cuban",
-        "Cypriot",
-        "Czech",
-        "Danish",
-        "Djibouti",
-        "Dominican",
-        "Dutch",
-        "East Timorese",
-        "Ecuadorean",
-        "Egyptian",
-        "Emirian",
-        "Equatorial Guinean",
-        "Eritrean",
-        "Estonian",
-        "Ethiopian",
-        "Fijian",
-        "Filipino",
-        "Finnish",
-        "French",
-        "Gabonese",
-        "Gambian",
-        "Georgian",
-        "German",
-        "Ghanaian",
-        "Greek",
-        "Grenadian",
-        "Guatemalan",
-        "Guinea-Bissauan",
-        "Guinean",
-        "Guyanese",
-        "Haitian",
-        "Herzegovinian",
-        "Honduran",
-        "Hungarian",
-        "I-Kiribati",
-        "Icelander",
-        "Indian",
-        "Indonesian",
-        "Iranian",
-        "Iraqi",
-        "Irish",
-        "Israeli",
-        "Italian",
-        "Ivorian",
-        "Jamaican",
-        "Japanese",
-        "Jordanian",
-        "Kazakhstani",
-        "Kenyan",
-        "Kittian and Nevisian",
-        "Kuwaiti",
-        "Kyrgyz",
-        "Laotian",
-        "Latvian",
-        "Lebanese",
-        "Liberian",
-        "Libyan",
-        "Liechtensteiner",
-        "Lithuanian",
-        "Luxembourger",
-        "Macedonian",
-        "Malagasy",
-        "Malawian",
-        "Malaysian",
-        "Maldivan",
-        "Malian",
-        "Maltese",
-        "Marshallese",
-        "Mauritanian",
-        "Mauritian",
-        "Mexican",
-        "Micronesian",
-        "Moldovan",
-        "Monacan",
-        "Mongolian",
-        "Moroccan",
-        "Mosotho",
-        "Motswana",
-        "Mozambican",
-        "Namibian",
-        "Nauruan",
-        "Nepalese",
-        "New Zealander",
-        "Nicaraguan",
-        "Nigerian",
-        "Nigerien",
-        "North Korean",
-        "Northern Irish",
-        "Norwegian",
-        "Omani",
-        "Pakistani",
-        "Palauan",
-        "Panamanian",
-        "Papua New Guinean",
-        "Paraguayan",
-        "Peruvian",
-        "Polish",
-        "Portuguese",
-        "Qatari",
-        "Romanian",
-        "Russian",
-        "Rwandan",
-        "Saint Lucian",
-        "Salvadoran",
-        "Samoan",
-        "San Marinese",
-        "Sao Tomean",
-        "Saudi",
-        "Scottish",
-        "Senegalese",
-        "Serbian",
-        "Seychellois",
-        "Sierra Leonean",
-        "Singaporean",
-        "Slovakian",
-        "Slovenian",
-        "Solomon Islander",
-        "Somali",
-        "South African",
-        "South Korean",
-        "Spanish",
-        "Sri Lankan",
-        "Sudanese",
-        "Surinamer",
-        "Swazi",
-        "Swedish",
-        "Swiss",
-        "Syrian",
-        "Taiwanese",
-        "Tajik",
-        "Tanzanian",
-        "Thai",
-        "Togolese",
-        "Tongan",
-        "Trinidadian or Tobagonian",
-        "Tunisian",
-        "Turkish",
-        "Tuvaluan",
-        "Ugandan",
-        "Ukrainian",
-        "Uruguayan",
-        "Uzbekistani",
-        "Venezuelan",
-        "Vietnamese",
-        "Welsh",
-        "Yemenite",
-        "Zambian",
-        "Zimbabwean"
-      ],
       openCard: false,
-      openCardEdit: false,
 
       film: '',
 
@@ -316,9 +66,6 @@ export default {
   methods: {
     openCardNewFilm: function() {
       this.openCard = true;
-    },
-    openCardEditFilm: function() {
-      this.openCardEdit = true;
     },
     addFilm: function() {
 
@@ -344,10 +91,6 @@ export default {
       this.langueFilm = '';
       this.genreFilm = '';
       this.openCard = false;
-    },
-    editFilm: function(index) {
-      this.films[index].titre = this.titreFilm;
-      this.openCardEdit = false;
     }
   }
 }
---
+++ src/components/FormEdit.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="bg-white text-dark rounded p-3 m-4" id="cardEditFilm" v-show="openCardEdit">
+    <span class="fw-bold">Éditer un film</span>
+    <input type="text" class="form-control mt-2 mb-3" v-model="film.titre" placeholder="Titre du film">
+    <select class="form-control mt-2 mb-3" v-model="film.genre">
+      <option disabled value="">Genre</option>
+      <option v-for="(genre) in genres" :value="genre">{{ genre }}</option>
+    </select>
+    <input type="text" class="form-control mt-2 mb-3" v-model="film.annee" placeholder="Année">
+    <select class="form-control mt-2 mb-3" v-model="film.langue">
+      <option disabled value="">Langue</option>
+      <option v-for="(langue) in langues" :value="langue">{{ langue }}</option>
+    </select>
+    <hr/>
+    <span class="fw-bold">Réalisateur</span>
+    <input type="text" class="form-control mt-2 mb-3" v-model="film.realisateur.nom" placeholder="Nom">
+    <input type="text" class="form-control mt-2 mb-3" v-model="film.realisateur.prenom" placeholder="Prénom">
+    <select class="form-control mt-2 mb-3" v-model="film.realisateur.nationalite">
+      <option disabled value="">Nationalité</option>
+      <option v-for="(nationalite) in nationalites" :value="nationalite">{{ nationalite }}</option>
+    </select>
+    <input type="text" class="form-control mt-2 mb-3" v-model="film.realisateur.dateNaissance" placeholder="Année de naissance">
+
+    <button class="btn btn-success w-100" @click="editFilm()">Enregistrer</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormEdit",
+  props: {
+    genres: {
+      type: Array,
+      default: null
+    },
+    langues: {
+      type: Array,
+      default: null
+    },
+    nationalites: {
+      type: Array,
+      default: null
+    },
+    film: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+      openCardEdit: false
+    }
+  },
+  methods: {
+    openCardEditFilm: function() {
+      this.openCardEdit = true;
+    },
+    editFilm: function() {
+      this.openCardEdit = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
